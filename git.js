65623345982e460c2aa6d3024fe8e8d094282511
Marcelo <marcelo.tateishi@aluno.senai.br>
2025-04-07 15:18:22 -0300

    Incluído ordenação da lista

--- git.js
+++ git.js
@@ -1,3 +1,5 @@
 let comidas = ['lasanha', 'pizza', 'sushi', 'hamburguer'];
 console.log(`Lista de todas as comidas: ${comidas}`);
 console.log(`Primeira comida: ${comidas[0]}`);
+comidas.sort()
+console.log(`Comidas ordenadas: ${comidas}`);
